4b347bfb8d8456bb43f95a4b87d98c5a3cef11ba
alexdresko <me@alexdresko.com>
2012-07-10 22:39:23 -0400

    Playing with branches in github 4 win

--- Dev/Asana.Posture.Web.Ui1/Controllers/AsanaController.cs
+++ Dev/Asana.Posture.Web.Ui1/Controllers/AsanaController.cs
@@ -1,33 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace NsTasks.Web.Ui1.Controllers
+﻿namespace NsTasks.Web.Ui1.Controllers
 {
+	using System.Web.Mvc;
+
 	using Asana.Posture.Core.Data;
 
 	public partial class AsanaController : Controller
-    {
-        //
-        // GET: /Asana/
-
-		public virtual ActionResult Index()
-        {
-            return View();
-        }
+	{
+		// GET: /Asana/
+		#region Public Methods and Operators
 
 		public virtual ActionResult GetAsanaNav()
 		{
 			var repository = new AsanaRespository();
-			return Json(repository.GetAsanaNav(), JsonRequestBehavior.AllowGet);
+
+			return this.Json(repository.GetAsanaNav(), JsonRequestBehavior.AllowGet);
 		}
 
 		public virtual ActionResult GetWorkspaceStats(long id)
 		{
 			var repository = new AsanaRespository();
-			return Json(repository.GetWorkspaceStats(id), JsonRequestBehavior.AllowGet);
+			return this.Json(repository.GetWorkspaceStats(id), JsonRequestBehavior.AllowGet);
 		}
-    }
+
+		public virtual ActionResult Index()
+		{
+			return this.View();
+		}
+
+		#endregion
+	}
 }
